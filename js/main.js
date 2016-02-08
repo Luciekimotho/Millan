c46707697289db810c73bd2cff8f61507e6bf1d9
Luciekimotho <luciekimotho@gmail.com>
2016-02-08 10:57:40 +0300

    Finished first version of the site

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 (function () {
 
     var documentEl = $(document) //new variable
@@ -8,4 +9,27 @@
         paralaxBg.css('background-position', '0' + -currScrollPos / 4 + 'px'); //the minus makes the background move one step up when we scroll.the zero means we dont change the margin.
     });
 
-})();
+    $( document ).ready(function() {
+            $('.dropdown-button').dropdown();
+            console.log("dropdown");
+        });
+
+})(jQuery);
+
+
+/*(function($) {
+		$(function() {
+
+$('.dropdown-button').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      hover: true, // Activate on hover
+      belowOrigin: true, // Displays dropdown below the button
+      alignment: 'right' // Displays dropdown with edge aligned to the left of button
+    }
+  );
+
+		}); // End Document Ready
+})(jQuery); // End of jQuery name space*/
+
+
